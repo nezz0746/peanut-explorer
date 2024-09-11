7e48a10b62c6ca35d308445a8313480c43a89752
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-09-11 17:23:34 +0200

    feat: deploy basic base peanut V3 subgraph

---
+++ packages/subgraph/src/peanutV4.ts
@@ -0,0 +1,140 @@
+import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import {
+  DepositEvent as DepositEventEvent,
+  MessageEvent as MessageEventEvent,
+  WithdrawEvent as WithdrawEventEvent,
+  PeanutV4,
+} from "../generated/PeanutV4/PeanutV4";
+import { ERC20 } from "../generated/PeanutV4/ERC20";
+import {
+  DepositEvent,
+  MessageEvent,
+  WithdrawEvent,
+  Deposit,
+  DepositTotals,
+} from "../generated/schema";
+
+let addressZero = "0x0000000000000000000000000000000000000000";
+
+export function handleDepositEvent(event: DepositEventEvent): void {
+  let entity = new DepositEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity._index = event.params._index;
+  entity._contractType = event.params._contractType;
+  entity._amount = event.params._amount;
+  entity._senderAddress = event.params._senderAddress;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+
+  let contract = PeanutV4.bind(event.address);
+
+  let deposit = Deposit.load(event.params._index.toString());
+
+  // If the deposit does not exist, create it
+  if (deposit == null) {
+    log.info("Deposit does not exist", []);
+
+    let contract_deposit = contract.getDeposit(event.params._index);
+
+    log.info("Creating deposit of amount:", [
+      contract_deposit.amount.toHexString(),
+    ]);
+
+    let deposit = new Deposit(event.params._index.toString());
+
+    deposit.amount = contract_deposit.amount;
+    deposit.tokenAddress = contract_deposit.tokenAddress;
+    deposit.contractType = contract_deposit.contractType;
+    deposit.claimed = contract_deposit.claimed;
+    deposit.requiresMFA = contract_deposit.requiresMFA;
+    deposit.timestamp = contract_deposit.timestamp;
+    deposit.tokenId = contract_deposit.tokenId;
+    deposit.senderAddress = contract_deposit.senderAddress;
+
+    deposit.save();
+    // Create addressZero variable
+
+    // if the deposit is an ERC20 or ETH
+    if (contract_deposit.contractType == 0) {
+      // Check if a DepositTotals entity exists with addresszero
+      let depositTotals = DepositTotals.load(addressZero);
+
+      // If it does not exist, create it
+      if (depositTotals == null) {
+        depositTotals = new DepositTotals(addressZero);
+        depositTotals.tokenAddress = contract_deposit.tokenAddress;
+        depositTotals.totalDeposited = contract_deposit.amount;
+        depositTotals.totalDeposists = BigInt.fromI32(1);
+      } else {
+        depositTotals.totalDeposited = depositTotals.totalDeposited.plus(
+          contract_deposit.amount,
+        );
+        depositTotals.totalDeposists = depositTotals.totalDeposists.plus(
+          BigInt.fromI32(1),
+        );
+      }
+      depositTotals.save();
+    } else if (contract_deposit.contractType == 1) {
+      // Check if a DepositTotals entity exists with the token address
+      let depositTotals = DepositTotals.load(
+        contract_deposit.tokenAddress.toHexString(),
+      );
+
+      // If it does not exist, create it
+      if (depositTotals == null) {
+        depositTotals = new DepositTotals(
+          contract_deposit.tokenAddress.toHexString(),
+        );
+        let erc20 = ERC20.bind(contract_deposit.tokenAddress);
+        depositTotals.tokenAddress = contract_deposit.tokenAddress;
+        depositTotals.totalDeposited = contract_deposit.amount;
+        depositTotals.name = erc20.name();
+        depositTotals.symbol = erc20.symbol();
+        depositTotals.decimals = erc20.decimals();
+        depositTotals.totalDeposists = BigInt.fromI32(1);
+      } else {
+        depositTotals.totalDeposited = depositTotals.totalDeposited.plus(
+          contract_deposit.amount,
+        );
+        depositTotals.totalDeposists = depositTotals.totalDeposists.plus(
+          BigInt.fromI32(1),
+        );
+      }
+      depositTotals.save();
+    }
+  }
+}
+
+export function handleMessageEvent(event: MessageEventEvent): void {
+  let entity = new MessageEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity.message = event.params.message;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleWithdrawEvent(event: WithdrawEventEvent): void {
+  let entity = new WithdrawEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity._index = event.params._index;
+  entity._contractType = event.params._contractType;
+  entity._amount = event.params._amount;
+  entity._recipientAddress = event.params._recipientAddress;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
