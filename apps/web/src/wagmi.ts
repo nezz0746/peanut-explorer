77ac2ba4956d0efc47a201ac05f06df9caaa1e4d
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-09-27 11:31:35 +0200

    feat: use custom wagmi hook to get ehters providers

--- apps/web/src/wagmi.ts
+++ apps/web/src/wagmi.ts
@@ -2,6 +2,10 @@ import { createConfig } from "@privy-io/wagmi";
 import { base, mainnet } from "viem/chains";
 import { http } from "wagmi";
 import { alchemyKey } from "./env";
+import { providers } from "ethers";
+import { useMemo } from "react";
+import type { Chain, Client, Transport } from "viem";
+import { Config, useClient } from "wagmi";
 
 export const wagmiConfig = createConfig({
   chains: [mainnet, base],
@@ -10,3 +14,30 @@ export const wagmiConfig = createConfig({
     [base.id]: http(`https://base-mainnet.g.alchemy.com/v2/${alchemyKey}`),
   },
 });
+
+export function clientToProvider(client: Client<Transport, Chain>) {
+  const { chain, transport } = client;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  if (transport.type === "fallback")
+    return new providers.FallbackProvider(
+      (transport.transports as ReturnType<Transport>[]).map(
+        ({ value }) => new providers.JsonRpcProvider(value?.url, network),
+      ),
+    );
+  return new providers.JsonRpcProvider(transport.url, network);
+}
+
+/** Hook to convert a viem Client to an ethers.js Provider. */
+export function useEthersProvider({
+  chainId,
+}: { chainId?: number | undefined } = {}) {
+  const client = useClient<Config>({ chainId });
+  return useMemo(
+    () => (client ? clientToProvider(client) : undefined),
+    [client],
+  );
+}
