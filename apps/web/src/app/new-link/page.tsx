668c57cecaaf5377e4ed2c5ec16960955c9c2ca6
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-09-27 12:22:18 +0200

    feat: add custom magic buttons & update layout navigation

---
+++ apps/web/src/app/new-link/page.tsx
@@ -0,0 +1,9 @@
+const NewLinkPage = () => {
+  return (
+    <div>
+      <h1>New Link</h1>
+    </div>
+  );
+};
+
+export default NewLinkPage;
--- apps/web/src/components/Layout.tsx
+++ apps/web/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Menu } from "lucide-react";
+import { LinkIcon, Menu } from "lucide-react";
 import { Button } from "@peanut/ui/components/ui/button";
 
 import {
@@ -19,6 +19,7 @@ import {
   SelectLabel,
   SelectTrigger,
 } from "@peanut/ui/components/ui/select";
+import PulsatingButton from "@peanut/ui/components/ui/shimmer-button";
 import {
   constants,
   SupportedChainsIds,
@@ -28,6 +29,7 @@ import { getChainImageURL } from "../helpers";
 import { HTMLAttributeAnchorTarget } from "react";
 import BlurryBackground from "./BlurryBackground";
 import { UserPill } from "@privy-io/react-auth/ui";
+import { useState } from "react";
 
 export const description =
   "An application shell with a header and main content area. The header has a navbar, a search input and and a user nav dropdown. The user nav is toggled by a button with an avatar image. The main content area is divided into two rows. The first row has a grid of cards with statistics. The second row has a grid of cards with a table of recent transactions and a list of recent sales.";
@@ -42,6 +44,7 @@ const networkNames: Record<SupportedChainsIds, string> = {
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const { setChainId, chainId: explorerChainId } = useExplorerChain();
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
 
   const links: {
     name: string;
@@ -54,6 +57,59 @@ export function Layout({ children }: { children: React.ReactNode }) {
       target: "_blank",
     },
   ];
+
+  const createLinkButton = (
+    <Link href={"/new-link"} onClick={() => setIsSheetOpen(false)}>
+      <PulsatingButton
+        shimmerSize="2px"
+        shimmerColor="#fff421"
+        borderRadius="10px"
+        className="bg-gray-200 w-full md:w-auto md:h-10 text-sm gap-2"
+      >
+        Create Link <LinkIcon className="w-[13px] h-[13px]" />
+      </PulsatingButton>
+    </Link>
+  );
+
+  const appChainSwitcher = (
+    <div className="flex flex-row items-center gap-2">
+      <Image
+        src={getChainImageURL(explorerChainId.toString())}
+        height={100}
+        width={100}
+        alt=""
+        className="w-6 h-6"
+      />
+      <Select
+        onValueChange={(value) => {
+          setChainId(parseInt(value) as SupportedChainsIds);
+        }}
+      >
+        <SelectTrigger className="w-[180px]">
+          {networkNames[explorerChainId]}
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Networks</SelectLabel>
+            {Object.keys(constants.subgraphURLs).map((chainId) => (
+              <SelectItem value={chainId} key={chainId}>
+                <div className="flex flex-row items-center justify-start gap-2">
+                  <Image
+                    src={getChainImageURL(chainId)}
+                    height={100}
+                    width={100}
+                    alt=""
+                    className="w-6 h-6"
+                  />
+                  {networkNames[parseInt(chainId) as SupportedChainsIds]}
+                </div>
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 
   return (
     <div className="flex min-h-screen w-full flex-col">
@@ -90,22 +146,26 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </Link>
           ))}
         </nav>
-        <Sheet>
+        <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
           <SheetTrigger asChild>
             <Button
               variant="outline"
               size="icon"
               className="shrink-0 md:hidden"
+              onClick={() => setIsSheetOpen(true)}
             >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <nav className="grid gap-6 text-lg font-medium">
+            <nav className="gap-6 flex flex-col text-lg font-medium h-[75%] ">
               <Link
-                href="#"
+                href="/"
                 className="flex items-center gap-2 text-lg font-semibold"
+                onClick={() => {
+                  setIsSheetOpen(false);
+                }}
               >
                 <Image
                   className="w-8 h-8"
@@ -118,16 +178,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
                   Peanut Explorer
                 </p>
               </Link>
-              {links.map((link) => (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  target={link.target ?? "_self"}
-                  className="hover:text-foreground text-nowrap"
-                >
-                  {link.name}
-                </Link>
-              ))}
+              <div className=" flex flex-col flex-grow justify-between ">
+                <div className="flex flex-col gap-2">
+                  {links.map((link) => (
+                    <Link
+                      key={link.href}
+                      href={link.href}
+                      target={link.target ?? "_self"}
+                      className="hover:text-foreground text-nowrap"
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                  {createLinkButton}
+                </div>
+                {appChainSwitcher}
+              </div>
             </nav>
           </SheetContent>
         </Sheet>
@@ -144,41 +210,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
           />
         </Link>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4 justify-end">
-          <Image
-            src={getChainImageURL(explorerChainId.toString())}
-            height={100}
-            width={100}
-            alt=""
-            className="w-6 h-6"
-          />
-          <Select
-            onValueChange={(value) => {
-              setChainId(parseInt(value) as SupportedChainsIds);
-            }}
-          >
-            <SelectTrigger className="w-[180px]">
-              {networkNames[explorerChainId]}
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Networks</SelectLabel>
-                {Object.keys(constants.subgraphURLs).map((chainId) => (
-                  <SelectItem value={chainId} key={chainId}>
-                    <div className="flex flex-row items-center justify-start gap-2">
-                      <Image
-                        src={getChainImageURL(chainId)}
-                        height={100}
-                        width={100}
-                        alt=""
-                        className="w-6 h-6"
-                      />
-                      {networkNames[parseInt(chainId) as SupportedChainsIds]}
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <div className="hidden md:flex">{appChainSwitcher}</div>
+          <div className="hidden md:flex">{createLinkButton}</div>
           <div className="[&_button]:bg-white [&_button]:text-black [&_button]:px-5 [&_button]:py-2 [&_button]:rounded-md [&_button]:border [&_button]:h-10">
             <UserPill label="Login" expanded />
           </div>
